a37e34c46d6e9a13a081834347a41f63dccce5a9
Kseniya Potapovich <ksuy.potapovich@bk.ru>
2023-12-10 20:11:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import RootPage from "./pages/RootPage";
+import './normilize.css'
+import RegisterForm from "./pages/RegisterPage";
+import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<RootPage/>}></Route>
+          <Route exact path="/SignUp" element={<RegisterForm/>}></Route>
+          <Route exact path="/login" element={<AuthPage/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
